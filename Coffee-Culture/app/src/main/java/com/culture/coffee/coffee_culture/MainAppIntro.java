8b3f13e361afd0613e889755e18acf1603a1b201
ImayaDismas <imayadismas@gmail.com>
2016-03-09 18:28:26 +0300

    Edited the project

--- Coffee-Culture/app/src/main/java/com/culture/coffee/coffee_culture/MainAppIntro.java
+++ Coffee-Culture/app/src/main/java/com/culture/coffee/coffee_culture/MainAppIntro.java
@@ -22,7 +22,7 @@ public class MainAppIntro extends AppIntro {
         addSlide(SampleSlide.newInstance(R.layout.first_fragment));
         addSlide(SampleSlide.newInstance(R.layout.second_fragment));
         addSlide(SampleSlide.newInstance(R.layout.third_fragment));
-        addSlide(SampleSlide.newInstance(R.layout.fourth_fragment));
+//        addSlide(SampleSlide.newInstance(R.layout.fourth_fragment));
 
         // Instead of fragments, you can also use our default slide
         // Just set a title, description, background and image. AppIntro will do the rest.
