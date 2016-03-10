ca6130a370e2b2b5596e7d00a81fdd876befb7e2
joseph muli <jayjaymuli00@gmail.com>
2016-03-10 19:19:58 +0300

    added activities

---
+++ Coffee-Culture/app/src/main/java/com/culture/coffee/coffee_culture/Java.java
@@ -0,0 +1,13 @@
+package com.culture.coffee.coffee_culture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Java extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.java);
+    }
+}
