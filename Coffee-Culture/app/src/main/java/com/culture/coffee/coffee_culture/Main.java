e007aa4b290d5aba84e3597ed229d60c715a1326
PhillMue <muenzuve@gmail.com>
2016-03-11 10:38:41 +0300

    added drawables

--- Coffee-Culture/app/src/main/java/com/culture/coffee/coffee_culture/Main.java
+++ Coffee-Culture/app/src/main/java/com/culture/coffee/coffee_culture/Main.java
@@ -1,11 +1,14 @@
 package com.culture.coffee.coffee_culture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.view.CardViewNative;
+import it.gmariotti.cardslib.library.internal.CardThumbnail;
 
 
 public class Main extends AppCompatActivity {
@@ -14,16 +17,30 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Create a Card
-        Card card = new Card(getApplicationContext());
+        // Create a Card
+        Card card = new Card(this, R.layout.row_card);
 
-        //Create a CardHeader
-        CardHeader header = new CardHeader(getApplicationContext());
+// Create a CardHeader
+        CardHeader header = new CardHeader(this);
+        header.setTitle("Hello world");
 
-        //Add Header to card
+        card.setTitle("Simple card demo");
+        CardThumbnail thumb = new CardThumbnail(this);
+        thumb.setDrawableResource(R.drawable.ic_launcher);
+
+        card.addCardThumbnail(thumb);
+
+// Add Header to card
         card.addCardHeader(header);
 
-        //Set card in the cardView
-        CardViewNative cardView = (CardViewNative) findViewById(R.id.carddemo);
+// Set card in the cardView
+        CardView cardView = (CardView) findViewById(R.id.carddemo);
         cardView.setCard(card);
+    }
+
+
+    public void btnCardListDemoClick(View v){
+        Intent intent = new Intent(this, CardListActivity.class);
+        startActivity(intent);
     }
 }
